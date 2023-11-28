abc08297b655330a155c80a651e6884be1145e0a
Abhishek <contact.ad18@gmail.com>
2023-11-28 17:10:08 +0530

    nc

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)b(8d+g)t5!=roal@=&yvh@e6wzv-h&gi!91v&)&1h$gym_78m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
